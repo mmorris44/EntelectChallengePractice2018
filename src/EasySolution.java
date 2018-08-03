0b13444f5a28501fecdfbe48d1fb42a27a53050e
Matthew Morris <MRRMAT011@myuct.ac.za>
2018-08-03 21:04:49 +0200

    Simple solution to test marker

--- src/EasySolution.java
+++ src/EasySolution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class EasySolution {
 
     public static void main (String[] args) {
@@ -6,7 +8,26 @@ public class EasySolution {
         InputParser ip = new InputParser();
         ip.read(inputFile);
 
-        
+        int nWorkers = ip.numberOfWorkers;
+
+        // Find all things to do
+        ArrayList<Character> route = new ArrayList<>();
+        for (int i = 0; i < ip.grid.size(); ++i) {
+            for (int j = 0; j < ip.grid.get(i).size(); ++j) {
+                Node node = ip.grid.get(i).get(j);
+                if (node instanceof Mine) {
+                    route.add(node.id);
+                    route.add(Character.toLowerCase(node.id));
+                }
+            }
+        }
+
+        // Ouput route
+        System.out.print(route.get(0));
+        for (int i = 1; i < route.size(); ++i) {
+            System.out.print("," + route.get(i));
+        }
+        System.out.println();
     }
 
 }
